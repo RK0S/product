fcb36087dd5a56ca0d782f9b2944cfd11a46b04f
Roman <r.l.kosolapov@gmail.com>
2023-05-19 15:50:34 +0300

    Добавлен итальянский язык

--- src/features/switchLanguage/UI/LangSwitcher.tsx
+++ src/features/switchLanguage/UI/LangSwitcher.tsx
@@ -1,25 +1,23 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { AppButton } from 'shared/UI/AppButton/AppButton';
+import { toggleLang } from '../lib/toggleLang/toggleLang';
 
 interface LangSwitcherProps {
     className?: string;
 }
 
-export const LangSwitcher: React.FC<LangSwitcherProps> = (props) => {
+export const LangSwitcher: React.FC<LangSwitcherProps> = props => {
     const { className } = props;
 
     const { t, i18n } = useTranslation();
 
     const toggle = async () => {
-        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
+        i18n.changeLanguage(toggleLang(i18n.language));
     };
 
     return (
-        <AppButton
-            className={classNames('', {}, [className])}
-            onClick={toggle}
-        >
+        <AppButton className={classNames('', {}, [className])} onClick={toggle}>
             {t('Language')}
         </AppButton>
     );
---
+++ src/features/switchLanguage/lib/toggleLang/toggleLang.ts
@@ -0,0 +1,12 @@
+type langType = 'ru' | 'en' | 'it';
+
+export const toggleLang = (lang: string): langType => {
+    switch (lang) {
+    case 'ru':
+        return 'en';
+    case 'en':
+        return 'it';
+    case 'it':
+        return 'ru';
+    }
+};
