e135d2b61c9e8a2e45aff37064c089aff07bf43c
Roman <r.l.kosolapov@gmail.com>
2023-05-29 18:11:35 +0300

    Написаны тесты и функция TestAsynkThunk

---
+++ src/features/authByUsername/model/selectors/getLoginState/getLoginState.test.ts
@@ -0,0 +1,31 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getLoginState } from './getLoginState';
+
+describe('getLoginState.test', () => {
+    test('return loginForm', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: {
+                username: '',
+                password: '',
+                isLoading: false,
+                error: 'error'
+            }
+        };
+        expect(getLoginState(state as StateSchema)).toEqual({
+            username: '',
+            password: '',
+            isLoading: false,
+            error: 'error'
+        });
+    });
+
+    test('empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getLoginState(state as StateSchema)).toEqual({
+            username: '',
+            password: '',
+            isLoading: false
+        });
+    });
+});
