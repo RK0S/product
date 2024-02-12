fd21d1bb17734a6009718352993d6fc7bd88e8d4
Roman <r.l.kosolapov@gmail.com>
2024-02-12 12:25:49 +0300

    add CopyPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,8 +5,15 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
 
-export function buildPlugins({ paths, isDev, analyze, apiUrl, project }: BuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugins({
+    paths,
+    isDev,
+    analyze,
+    apiUrl,
+    project
+}: BuildOptions): webpack.WebpackPluginInstance[] {
     return [
         new HTMLWebpackPlugin({
             template: paths.html
@@ -19,7 +26,10 @@ export function buildPlugins({ paths, isDev, analyze, apiUrl, project }: BuildOp
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
             __API__: JSON.stringify(apiUrl),
-            __PROJECT__: JSON.stringify(project),
+            __PROJECT__: JSON.stringify(project)
+        }),
+        new CopyPlugin({
+            patterns: [{ from: paths.locales, to: paths.buildLocales }]
         }),
         ...(isDev ? [new ReactRefreshWebpackPlugin(), new ForkTsCheckerWebpackPlugin()] : []),
         new BundleAnalyzerPlugin({
